55995673b6605b038beb167b0c0847567c8878bd
Anju Vishwakarma <0123anjuvish@gmail.com>
2023-01-16 22:46:02 +0530

    update

--- src/components/pages/Catalog.jsx
+++ src/components/pages/Catalog.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import { FaMobileAlt } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
 //import {HiOutlineShoppingCart } from "react-icons/fa";
@@ -6,9 +8,9 @@ import { FaSearch } from "react-icons/fa";
 const Catalog = ()=>{
     return(
 <>
-<div className="">
-  hello catalog
-</div>
+<Box component="span" sx={{ p: 2, border: '1px dashed grey' }}>
+      <Button>Save</Button>
+    </Box>
 </>
     )
 }
